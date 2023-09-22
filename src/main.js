f5900593212b12263689d4f737c0a0eae410a346
CyndelHerolt <herolt.cyndel@gmail.com>
2023-09-22 15:48:47 +0200

    feat [Structure] new structure

--- src/main.js
+++ src/main.js
@@ -3,9 +3,26 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faQuestionSquare } from '@fortawesome/pro-light-svg-icons'
+import { faHouseHeart } from '@fortawesome/pro-light-svg-icons'
+import { faCompactDisc } from '@fortawesome/pro-light-svg-icons'
+import { faStar } from '@fortawesome/pro-light-svg-icons'
+import { faBalloons } from '@fortawesome/pro-light-svg-icons'
+import { faUser } from '@fortawesome/pro-light-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 const app = createApp(App)
 
 app.use(router)
+
+library.add(faQuestionSquare)
+library.add(faHouseHeart)
+library.add(faCompactDisc)
+library.add(faStar)
+library.add(faBalloons)
+library.add(faUser)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
