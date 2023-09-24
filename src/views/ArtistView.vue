f98eb3875aa89f33c04b3c027183f1fb543215b8
CyndelHerolt <herolt.cyndel@gmail.com>
2023-09-24 15:18:53 +0200

    feat [API] Fiche Artist

--- src/views/ArtistView.vue
+++ src/views/ArtistView.vue
@@ -6,6 +6,8 @@ import axios from "axios";
 const route = useRoute()
 const id = route.params.id
 const artist = ref('')
+const album = ref('')
+const topTrack = ref('')
 let token = '' // Declare a variable for the token
 
 onMounted(async () => {
@@ -14,22 +16,71 @@ onMounted(async () => {
   token = tokenResponse.data.access_token;
 
   // Include the token in the headers of your axios request
-  const response = await axios.get('https://api.spotify.com/v1/artists/' + id, {
+  const artists = await axios.get('https://api.spotify.com/v1/artists/' + id, {
     headers: {
       'Authorization': `Bearer ${token}`,
     },
   })
+  artist.value = artists.data
 
-  artist.value = response.data
+  const albums = await axios.get('https://api.spotify.com/v1/artists/' + id + '/albums', {
+    headers: {
+      'Authorization': `Bearer ${token}`,
+    },
+  })
+  album.value = albums.data
+
+  const topTracks = await axios.get('https://api.spotify.com/v1/artists/' + id + '/top-tracks?market=FR', {
+    headers: {
+      'Authorization': `Bearer ${token}`,
+    },
+  })
+  topTrack.value = topTracks.data
 })
 </script>
 <template>
+  <section>
+    <div class="artist-header">
+      <img :src="artist.images?.[0]?.url" :alt="artist.name">
+      <h2>{{ artist.name }}</h2>
+      <ul>
+        <li v-for="genre in artist.genres">{{ genre }}</li>
+      </ul>
+    </div>
+    <hr>
+    <h2>Top Tracks</h2>
+    <!--  <pre>-->
+    <!--    {{ topTrack }}-->
+    <!--  </pre>-->
+    <ul>
+      <li v-for="track in topTrack.tracks">
+        <img :src="track.album.images?.[1]?.url" :alt="track.name">
+        <h4>{{ track.name }}</h4>
+        <p>{{ track.album.name }}</p>
+        <p>Nombre de pistes : {{ track.album.total_tracks }}</p>
+        <p>Popularité : {{ track.popularity }}</p>
+        <ul>
+          artistes
+          <li v-for="artist in track.artists">{{ artist.name }}</li>
+        </ul>
+      </li>
+    </ul>
+    <hr>
+    <h2>Albums</h2>
+    <ul>
+      <li v-for="album in album.items">
+        <img :src="album.images?.[1]?.url" :alt="album.name">
+        <h4>{{ album.name }}</h4>
+        <p>{{ album.album_type }}</p>
+        <p>Date de sortie : {{ album.release_date }}</p>
+        <p>Nombre de titres : {{ album.total_tracks }}</p>
+        <ul>
+          artistes
+          <li v-for="artist in album.artists">{{ artist.name }}</li>
+        </ul>
+      </li>
+    </ul>
 
-  <img :src="artist.images?.[0]?.url" :alt="artist.name">
-  <h2>{{ artist.name }}</h2>
-  <ul>
-    <li v-for="genre in artist.genres">{{ genre }}</li>
-  </ul>
-  <hr>
-  <pre>{{ artist }}</pre>
+  </section>
+  <!--  <pre>{{ album }}</pre>-->
 </template>
